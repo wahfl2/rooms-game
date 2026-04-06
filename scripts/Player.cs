99ddebfd2d8d07a4af9b8d0e6161a96a5235848c
ConfiG <serious@cwonfig.pet>
2026-04-07 01:55:09 +0300

    fix editorconfig

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,5 +1,4 @@
 using Godot;
-
 using System;
 
 public partial class Player : CharacterBody3D {
@@ -10,16 +9,20 @@ public partial class Player : CharacterBody3D {
         Vector3 velocity = Velocity;
 
         // Add the gravity.
-        if(!IsOnFloor()) { velocity += GetGravity() * (float)delta; }
+        if (!IsOnFloor()) {
+            velocity += GetGravity() * (float)delta;
+        }
 
         // Handle Jump.
-        if(Input.IsActionJustPressed("ui_accept") && IsOnFloor()) { velocity.Y = JumpVelocity; }
+        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()) {
+            velocity.Y = JumpVelocity;
+        }
 
         // Get the input direction and handle the movement/deceleration.
         // As good practice, you should replace UI actions with custom gameplay actions.
         Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
         Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-        if(direction != Vector3.Zero) {
+        if (direction != Vector3.Zero) {
             velocity.X = direction.X * Speed;
             velocity.Z = direction.Z * Speed;
         }
