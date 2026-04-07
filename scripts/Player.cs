2fee7d4e632d82dbe6ad1f039f70dfbfc5824e6e
wahfl2 <59855656+wahfl2@users.noreply.github.com>
2026-04-06 19:10:04 -0500

    movement stuff

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -2,36 +2,64 @@ using Godot;
 using System;
 
 public partial class Player : CharacterBody3D {
-    public const float Speed = 5.0f;
-    public const float JumpVelocity = 4.5f;
+    [Export] private float _movementSpeed = 5.0f;
+    [Export] private float _jumpVelocity = 4.5f;
+
+    [Export] private Camera3D _camera;
+    [Export] private float _cameraSensitivity;
+
+    private Vector2 _cameraRotation = new Vector2();
+
+    public override void _Ready() {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
+
+    public override void _Process(double delta) {
+        _camera.Rotation = _camera.Rotation with {
+            X = _cameraRotation.X,
+            Y = _cameraRotation.Y
+        };
+    }
 
     public override void _PhysicsProcess(double delta) {
         Vector3 velocity = Velocity;
 
-        // Add the gravity.
         if (!IsOnFloor()) {
             velocity += GetGravity() * (float)delta;
         }
 
-        // Handle Jump.
         if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()) {
-            velocity.Y = JumpVelocity;
+            velocity.Y = _jumpVelocity;
         }
 
-        // Get the input direction and handle the movement/deceleration.
-        // As good practice, you should replace UI actions with custom gameplay actions.
-        Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
+        inputDir = inputDir.Rotated(-_cameraRotation.Y);
+
         Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
         if (direction != Vector3.Zero) {
-            velocity.X = direction.X * Speed;
-            velocity.Z = direction.Z * Speed;
+            velocity.X = direction.X * _movementSpeed;
+            velocity.Z = direction.Z * _movementSpeed;
         }
         else {
-            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, _movementSpeed);
+            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, _movementSpeed);
         }
 
         Velocity = velocity;
         MoveAndSlide();
+    }
+
+    public override void _Input(InputEvent @event) {
+        if (@event is InputEventMouseMotion motion) {
+            _cameraRotation.X -= motion.Relative.Y * _cameraSensitivity;
+            _cameraRotation.Y -= motion.Relative.X * _cameraSensitivity;
+        }
+
+        if (@event.IsActionPressed("ui_cancel")) {
+            Input.MouseMode = Input.MouseMode switch {
+                Input.MouseModeEnum.Captured => Input.MouseModeEnum.Visible,
+                _ => Input.MouseModeEnum.Captured
+            };
+        }
     }
 }
